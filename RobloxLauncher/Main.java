482c8826eaa0308bc0a0ad2fc2a9e4384ef69bb6
Jay <jacksongardiner311@gmail.com>
2026-07-06 16:36:58 +1000

    Added icon.png

--- RobloxLauncher/Main.java
+++ RobloxLauncher/Main.java
@@ -8,16 +8,19 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLayout(null); // manual positioning, like before
 
+        ImageIcon icon = new ImageIcon("RobloxLauncher/images/icon.png");
+        window.setIconImage(icon.getImage());
+
         System.out.println("Looking in: " + new java.io.File("RobloxLauncher/images/background.png").getAbsolutePath()); //debug line to check the path
 
         // Load the image and put it in a label
-        ImageIcon backgroundImage = new ImageIcon("RobloxLauncher/images/background.png"); // error on this line needs fixing -J
+        ImageIcon backgroundImage = new ImageIcon("RobloxLauncher/images/background.png");
         System.out.println("Image width: " + backgroundImage.getIconWidth());
         JLabel background = new JLabel(backgroundImage);
         background.setBounds(0, 0, 500, 400); // covers the entire window
         window.add(background);
 
-        JButton launchButton = new JButton("LAUNCH ROADBLOCKS!!!    ");
+        JButton launchButton = new JButton("LAUNCH ROADBLOCKS!!!");
         launchButton.setBounds(130, 130, 140, 35);
         window.add(launchButton);
         
